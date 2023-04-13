19f01e76f43d9373ce1b9e1dabd23dc05187bd60
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2023-04-13 14:21:09 -0700

    added management folder for createsuperuser2

--- build.sh
+++ build.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-if [[ $CREATE_SUPERUSER ]];
-then python manage.py createsuperuser --no-input --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD
