8efe027b06e8e93e478f0714ddfe5a415744006c
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2023-04-13 23:01:17 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
-then python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD fi
+then
+  python manage.py createsuperuser --no-input
+fi
