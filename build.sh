c96f90fb7edeffb34506009de0706cf824a36ac6
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2023-04-13 22:47:20 -0700

    update build

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsu fi
+if [[ $CREATE_SUPERUSER ]];
+then python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD fi
